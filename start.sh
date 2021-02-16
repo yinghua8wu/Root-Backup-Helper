88ae226e47f43809b4723b1c6b8787bceaa05210
yinghua8wu <38562798+yinghua8wu@users.noreply.github.com>
2021-02-16 19:13:18 +0800

    Update start.sh
    23

--- start.sh
+++ start.sh
@@ -4,6 +4,6 @@ pkg update
 pkg upgrade
 pkg install curl
 pkg install git
-curl https://del.dog/raw/mtk-su.sh > mtk-su.sh
+curl https://raw.githubusercontent.com/yinghua8wu/Root-Backup-Helper/master/mtk-su.sh > mtk-su.sh
 chmod +x mtk-su.sh
 ./mtk-su.sh
